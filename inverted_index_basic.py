aeccbba0cd5d68d8c0b03f74cf9fb5972e925905
lion137 <lion137@gmail.com>
2018-09-18 23:41:10 +0100

    Initial

---
+++ inverted_index_basic.py
@@ -0,0 +1,83 @@
+# information retrieval project, tests on text sample, building inverted index,
+# preprocessed text, using nltk
+
+"""2018 Copyleft lion137"""
+
+
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+
+
+def tokenize(text):
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+    tokens = [t.lower() for t in tokens]
+    return tokens
+
+
+def remove_duplicates(xs):
+    s = []
+    for x in xs:
+        if x not in s:
+            s.append(x)
+    return s
+
+
+def make_inv_index_as_dict(xs):
+    ls = []
+    d = []
+    t_el = xs[0][0]
+    ls.append(xs[0][1])
+    t_f = 1
+    limit = len(xs)
+    if limit == 1:
+        d.append([[t_el, t_f], ls])
+        return d
+    for i in range(1, limit):
+        if t_el == xs[i][0]:
+            ls.append(xs[i][1])
+            t_f += 1
+            if i == limit - 1:
+                d.append([[t_el, t_f], ls])
+                return d
+            continue
+        d.append([[t_el, t_f], ls])
+        #d.append([[xs[i][0], 1], [xs[i][1]]])
+        ls =[]
+        t_f = 0
+        t_el = xs[i][0]
+        t_f = 1
+        ls.append(xs[i][1])
+        if i == limit - 1:
+            d.append([[t_el, t_f], ls])
+    return d
+
+
+def term_doc_id_list(xs):
+    ys = []
+    i = 1
+    for ind, doc in enumerate(xs):
+        ys += list(map(lambda x: [x, i], doc))
+        i += 1
+    return ys
+
+
+
+if __name__ == '__main__':
+    doc = """I did enact Julius Caesar: I was killed
+i’ the Capitol; Brutus killed me"""
+    doc1 = """So let it be with Caesar.  The noble Brutus
+hath told you Caesar was ambitious"""
+    docs = [doc, doc1]
+    docs = map(tokenize, docs)
+    docs = term_doc_id_list(docs)
+    docs = sorted(docs)
+    docs = remove_duplicates(docs)
+    print(make_inv_index_as_dict(docs))
+
+
+
+
+
+
+
+
