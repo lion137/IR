8ef7615bb2ed18ca999ba24fb75cef711ef1307c
lion137 <lion137@gmail.com>
2018-11-03 00:20:27 +0000

    imports

--- imports.py
+++ imports.py
@@ -1,12 +1,14 @@
 # imports:
 
 import string
-import re
 import operator as op
 from collections import defaultdict, OrderedDict
 from nltk.tokenize import word_tokenize, RegexpTokenizer
 import functools
 from itertools import product
+from functools import reduce
+from operator import add
+import requests, bs4, re, webbrowser
 
 
 
